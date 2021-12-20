83d2ce72b502bd90d7a554ed9ca38b2c05706cd4
jchen302023 <jchen30@stuy.edu>
2021-12-20 13:19:06 -0500

    wee

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,11 +1,13 @@
 public class SearchDriver {
 
-  public static long BinTime(Comparable[] a, int b) {
+  public static long BinTime(Comparable[] a, int b, int c) {
     long BinStart = System.currentTimeMillis();
-    BinSearch.binSearch(a, b);
+    for (int i = 0; i <  c; i ++) {
+          BinSearch.binSearch(a, b);
+    } //
     long BinEnd = System.currentTimeMillis();
     long BinTot = BinEnd - BinStart;
-    // System.out.println("Time it took: " + BinTot + " milliseconds");
+    System.out.println("Time it took: " + BinTot + " milliseconds");
     return BinTot;
   } // BinTime
 
@@ -26,14 +28,15 @@ public class SearchDriver {
       Test1[i] = i;
     }
 
-    BinTime(Test1, 1000000);
-    LinTime(Test1, 1000000);
-    BinTime(Test1, 80001);
-    LinTime(Test1, 80001);
+    int randomInt = (int) (Math.random() * (Test1.length + 1));
+    BinTime(Test1, randomInt, 1000000);
+
+    /*
     long LinAverage = (LinTime(Test1, 80001) + LinTime(Test1, 80001) + LinTime(Test1, 80001) + LinTime(Test1, 80001) + LinTime(Test1, 80001)) / 5;
     System.out.println("Average time for LinTime: " + LinAverage + " milliseconds");
     long BinAverage = (BinTime(Test1, 80001) + BinTime(Test1, 80001) + BinTime(Test1, 80001) + BinTime(Test1, 80001) + BinTime(Test1, 80001)) / 5;
     System.out.println("Average time for BinTime: " + BinAverage + " milliseconds");
+    */
 
 
 
