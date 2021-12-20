2e04a2ff5c95a18692c448c4148b5a87cf3b8eed
jchen302023 <jchen30@stuy.edu>
2021-12-20 16:54:53 -0500

    made changes

--- SearchDriver.java
+++ SearchDriver.java
@@ -7,29 +7,43 @@ public class SearchDriver {
     } //
     long BinEnd = System.currentTimeMillis();
     long BinTot = BinEnd - BinStart;
-    System.out.println("Time it took: " + BinTot + " milliseconds");
+    System.out.println("Time it took Bin: " + BinTot + " milliseconds");
     return BinTot;
   } // BinTime
 
-  public static long LinTime(Comparable[] a, int b) {
+  public static long LinTime(Comparable[] a, int b, int c) {
     long LinStart = System.currentTimeMillis();
-    LinSearch.linSearch(a, b);
+    for (int i = 0; i <  c; i ++) {
+          LinSearch.linSearch(a, b);
+    } //
     long LinEnd = System.currentTimeMillis();
     long LinTot = LinEnd - LinStart;
-    // System.out.println("Time it took: " + LinTot + " milliseconds");
+    System.out.println("Time it took Lin: " + LinTot + " milliseconds");
     return LinTot;
   } // LinTime
 
   public static void main(String[] args) {
     BinSearch BinTrial = new BinSearch();
     LinSearch LinTrail = new LinSearch();
-    Comparable[] Test1 = new Integer[100000000];
+    Comparable[] Test1 = new Integer[1000000]; // had to shorten by 2 zero since my computer's going to break
     for (int i = 0; i < Test1.length; i ++) {
       Test1[i] = i;
     }
+    Comparable[] Test2 = new Integer[100000];
+    for (int i = 0; i < Test2.length; i ++) {
+      Test2[i] = i;
+    }
 
     int randomInt = (int) (Math.random() * (Test1.length + 1));
-    BinTime(Test1, randomInt, 1000000);
+    System.out.println("Searching for... " + randomInt);
+    BinTime(Test1, randomInt, 10000);
+    LinTime(Test1, randomInt, 10000);
+
+    int randomInt2 = (int) (Math.random() * (Test2.length + 1));
+    System.out.println("Searching for... " + randomInt2);
+    BinTime(Test2, randomInt2, 10000);
+    LinTime(Test2, randomInt2, 10000);
+
 
     /*
     long LinAverage = (LinTime(Test1, 80001) + LinTime(Test1, 80001) + LinTime(Test1, 80001) + LinTime(Test1, 80001) + LinTime(Test1, 80001)) / 5;
